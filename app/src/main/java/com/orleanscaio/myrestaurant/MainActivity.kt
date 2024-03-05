3876394a924bee7d3ac702fe49a6409ad4958a42
Caio Orleans <caioorleansdm@gmail.com>
2024-03-05 14:22:11 -0300

    Options menu created

--- app/src/main/java/com/orleanscaio/myrestaurant/MainActivity.kt
+++ app/src/main/java/com/orleanscaio/myrestaurant/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.orleanscaio.myrestaurant
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View.OnCreateContextMenuListener
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.orleanscaio.myrestaurant.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main_activity, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.item_cart -> {
+                Toast.makeText(this, "Item de cart clicado", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
         }
     }
 }
