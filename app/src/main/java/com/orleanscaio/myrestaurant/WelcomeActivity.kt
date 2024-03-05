d98f01cb591fdb3e22c74c274bf2853890b1b7c1
Caio Orleans <caioorleansdm@gmail.com>
2024-03-04 22:11:01 -0300

    Navigation to main activity implemented

--- app/src/main/java/com/orleanscaio/myrestaurant/WelcomeActivity.kt
+++ app/src/main/java/com/orleanscaio/myrestaurant/WelcomeActivity.kt
@@ -1,21 +1,32 @@
 package com.orleanscaio.myrestaurant
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.orleanscaio.myrestaurant.databinding.ActivityWelcomeBinding
 
-class MainActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.welcome_activity)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnNext.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
         }
     }
 }
